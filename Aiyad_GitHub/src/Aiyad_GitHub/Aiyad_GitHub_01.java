8892612dd02ad2906c7f7529c0ae89b6a152c58f
aiyad <aiyad@DESKTOP-T33DO35>
2017-10-29 17:53:07 -0400

    Test1

---
+++ Aiyad_GitHub/src/Aiyad_GitHub/Aiyad_GitHub_01.java
@@ -0,0 +1,14 @@
+package Aiyad_GitHub;
+
+public class Aiyad_GitHub_01 {  // https://github.com/AiyadAhmed/Aiyad01.git
+
+	public static void main (String args[]){
+		
+		System.out.println("My first GIT project");
+		
+		
+		
+	}
+	
+	
+} // class
