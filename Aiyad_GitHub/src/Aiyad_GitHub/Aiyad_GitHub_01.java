0f9a17455ba43442ed45d64e9a177ea53995a5c3
aiyad <aiyad@DESKTOP-T33DO35>
2017-10-29 18:14:04 -0400

    tee

--- Aiyad_GitHub/src/Aiyad_GitHub/Aiyad_GitHub_01.java
+++ Aiyad_GitHub/src/Aiyad_GitHub/Aiyad_GitHub_01.java
@@ -7,8 +7,8 @@ public class Aiyad_GitHub_01 {  // https://github.com/AiyadAhmed/Aiyad01.git
 		System.out.println("My first GIT project");
 		
 		
-		
-	}
+		System.out.println("Welcome to my World");
+	} //main
 	
 	
 } // class
