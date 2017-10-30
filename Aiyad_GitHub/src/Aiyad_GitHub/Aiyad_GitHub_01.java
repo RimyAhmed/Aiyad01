6f774b1a3083d7dc8a49ba53b617a3cb0dc19313
aiyad <aiyad@DESKTOP-T33DO35>
2017-10-30 00:24:08 -0400

    Java Practice

--- Aiyad_GitHub/src/Aiyad_GitHub/Aiyad_GitHub_01.java
+++ Aiyad_GitHub/src/Aiyad_GitHub/Aiyad_GitHub_01.java
@@ -1,13 +1,42 @@
 package Aiyad_GitHub;
 
+import java.util.Scanner;
+
 public class Aiyad_GitHub_01 {  // https://github.com/AiyadAhmed/Aiyad01.git
 
 	public static void main (String args[]){
 		
 		System.out.println("My first GIT project");
+		System.out.println("Welcome to my IT World"+"\n");
 		
+		int g []= new int [5];
 		
-		System.out.println("Welcome to my World");
+		g [1]= 80;
+		g [2]= 70;
+		g [3]= 50;
+		g [4]= 40;
+		
+	
+		
+	for (int i =1; i<g.length; i++){
+		
+		System.out.println("Enter Your id num .....");
+		Scanner j = new Scanner (System.in);
+		String u = j.nextLine();
+		
+		if ( g[i] == 80 ){
+			System.out.println("Your Result is Grade A "+"\n");
+		}
+		if ( g[i] == 70 ){
+			System.out.println("Your Result is Grade B "+"\n");
+		}
+		if ( g[i] == 50 ){
+			System.out.println("Your Result is Grade C "+"\n");
+		}
+		if ( g[i] == 40 ){
+			System.out.println("Your Result is Grade D "+"\n");
+		}
+	} //loop
 		
 	} //main
 	
